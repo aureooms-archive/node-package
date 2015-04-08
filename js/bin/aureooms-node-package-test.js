59aaefc537171c6bf1aeff77daf946515a54c724
aureooms <aurelien.ooms@gmail.com>
2015-04-08 14:19:31 +0200

    require babel polyfill if needed

--- js/bin/aureooms-node-package-test.js
+++ js/bin/aureooms-node-package-test.js
@@ -8,4 +8,6 @@ var config = pkg.config;
 var data = fs.readFileSync(config, 'utf8');
 var opt = JSON.parse(data);
 
+if ( opt.babel ) require( "babel-core/polyfill" ) ;
+
 pkg.test(opt.name, opt.code.main, opt.code.test);
