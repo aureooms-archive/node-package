b714175c30159a6369dc26b7bf478fc105272678
aureooms <aurelien.ooms@gmail.com>
2015-04-08 12:01:33 +0200

    enable babel and build

--- js/dist/pkg.js
+++ js/dist/pkg.js
@@ -1,180 +1,182 @@
-(function(exports, undefined){
+'use strict';
+
+(function (exports, undefined) {
 
 	'use strict';
 
+	/* js/src/build.js */
 
-/* js/src/build.js */
+	var build = function build(opt) {
 
+		var fs = require('fs');
+		var util = require('util');
+		var fmt = util.format;
+		var recbuild_t = require('aureooms-node-recursive-build');
+		var UglifyJS = require('uglify-js');
+		var babel = require('babel-core');
+		var extend = require('node.extend');
+		var path = require('path');
 
-var build = function(opt){
+		var dflt = {
+			name: undefined,
+			src: undefined,
+			out: undefined,
+			base: 0,
+			rec: false,
+			flat: true,
+			babel: false
+		};
 
-	var fs = require('fs');
-	var util = require('util');
-	var fmt = util.format;
-	var recbuild_t = require('aureooms-node-recursive-build');
-	var UglifyJS = require('uglify-js');
-	var extend = require('node.extend');
+		opt = extend({}, dflt, opt);
 
-	var dflt = {
-		name : undefined,
-		src  : undefined,
-		out  : undefined,
-		base : 0,
-		rec  : false,
-		flat : true
-	};
+		var namespace = opt.name.replace('-', '');
 
-	opt = extend({}, dflt, opt);
+		var recbuild = recbuild_t({
+			name: namespace,
+			rec: opt.rec,
+			flat: opt.flat
+		});
 
-	var namespace = opt.name.replace( '-' , '' ) ;
+		if (!fs.existsSync(opt.out)) fs.mkdirSync(opt.out);
 
-	var recbuild = recbuild_t({
-		name : namespace ,
-		rec  : opt.rec ,
-		flat : opt.flat
-	});
+		var fd,
+		    base = fmt('%s/%s%%s', opt.out, opt.name),
+		    concat = path.normalize(fmt(base, '.js')),
+		    min = path.normalize(fmt(base, '.min.js')),
+		    map = path.normalize(fmt(base, '.js.map'));
 
-	if (!fs.existsSync(opt.out)) fs.mkdirSync(opt.out);
+		var fhandle = function fhandle(f) {
+			var raw = fs.readFileSync(f);
+			fs.writeSync(fd, raw.toString());
+			fs.writeSync(fd, '\n');
+		};
 
-	var fd,
-	    base = fmt('%s/%s%%s', opt.out, opt.name),
-	    concat = fmt(base, '.js'),
-	    min = fmt(base, '.min.js'),
-	    map = fmt(base, '.js.map');
+		var rhandle = function rhandle(raw) {
+			fs.writeSync(fd, raw);
+			fs.writeSync(fd, '\n');
+		};
 
-	var fhandle = function(f) {
-		var raw = fs.readFileSync(f);
-		fs.writeSync(fd, raw.toString());
-		fs.writeSync(fd, '\n');
-	};
+		fd = fs.openSync(concat, 'w');
+		recbuild(opt.src, namespace, -opt.base, fhandle, rhandle);
+		fs.closeSync(fd);
 
-	var rhandle = function(raw) {
-		fs.writeSync(fd, raw);
-		fs.writeSync(fd, '\n');
-	};
+		if (opt.babel) {
 
-	fd = fs.openSync(concat, 'w');
-	recbuild(opt.src, namespace, -opt.base, fhandle, rhandle);
-	fs.closeSync(fd);
+			var es5 = babel.transformFileSync(concat);
 
+			fd = fs.openSync(concat, 'w');
 
-	var minified = UglifyJS.minify(concat, { outSourceMap: map });
+			fs.writeSync(fd, es5.code);
 
-	fs.writeFileSync(min, minified.code);
-	fs.writeFileSync(map, minified.map);
-
-};
-
-exports.build = build;
-
-/* js/src/config.js */
-
-exports.config = 'pkg.json';
-
-/* js/src/include.js */
-
-
-
-var include = function(opt, handler){
-
-	var recquire_t = require('aureooms-node-recursive-require');
-	var extend = require('node.extend');
-
-	var dflt = {
-		name    : undefined,
-		src     : undefined,
-		exports : undefined,
-		base    : 0,
-		index   : 'index.js',
-		intro   : 'intro.js',
-		outro   : 'outro.js',
-		rec     : false,
-		flat    : true,
-		debug   : false
-	};
-
-	opt = extend({}, dflt, opt);
-
-	if ( opt.name ) opt.name = opt.name.replace( '-' , '' ) ;
-
-	var recquire = recquire_t(opt);
-
-	recquire(opt.src, opt.exports, -opt.base, handler);
-
-};
-
-
-exports.include = include;
-
-/* js/src/list.js */
-
-
-var list = function(opt){
-
-	var actions = [];
-	var handler = function(){ actions.push(arguments); };
-	include(opt, handler);
-	return actions;
-
-};
-
-
-exports.list = list;
-/* js/src/test.js */
-
-
-var test = function(name, codepath, testpath){
-
-	var path = require('path');
-	var argv = require('optimist').argv;
-	var testrunner = require('qunit');
-
-	testrunner.options.maxBlockDuration = 20000;
-	testrunner.options.coverage = true;
-	testrunner.options.log.tests = false;
-	testrunner.options.log.testing = false;
-	testrunner.options.log.assertions = false;
-	testrunner.options.log.summary = false;
-
-	var n = argv._.length || 1;
-	var failed = 0;
-	var done = 0;
-
-	var cb = function(err, report) {
-		if (err) {
-			console.log('error : some weird error happened, the only clue we have is this error message -> ' + err.message);
-			process.exit(-1);
+			fs.closeSync(fd);
 		}
 
-		failed += report.failed;
-		++done;
-		if (done === n) process.exit(failed);
+		var minified = UglifyJS.minify(concat, { outSourceMap: map });
+
+		fs.writeFileSync(min, minified.code);
+		fs.writeFileSync(map, minified.map);
 	};
 
-	var run = function(item) {
-		testrunner.run(
-			{
-				code : {
-					path : path.normalize(codepath.join('/')),
-					namespace : name.replace( '-' , '' )
+	exports.build = build;
+
+	/* js/src/config.js */
+
+	exports.config = 'pkg.json';
+
+	/* js/src/include.js */
+
+	var include = function include(opt, handler) {
+
+		var recquire_t = require('aureooms-node-recursive-require');
+		var extend = require('node.extend');
+
+		var dflt = {
+			name: undefined,
+			src: undefined,
+			exports: undefined,
+			base: 0,
+			index: 'index.js',
+			intro: 'intro.js',
+			outro: 'outro.js',
+			rec: false,
+			flat: true,
+			debug: false
+		};
+
+		opt = extend({}, dflt, opt);
+
+		if (opt.name) opt.name = opt.name.replace('-', '');
+
+		var recquire = recquire_t(opt);
+
+		recquire(opt.src, opt.exports, -opt.base, handler);
+	};
+
+	exports.include = include;
+
+	/* js/src/list.js */
+
+	var list = function list(opt) {
+
+		var actions = [];
+		var handler = function handler() {
+			actions.push(arguments);
+		};
+		include(opt, handler);
+		return actions;
+	};
+
+	exports.list = list;
+	/* js/src/test.js */
+
+	var test = function test(name, codepath, testpath) {
+
+		var path = require('path');
+		var argv = require('optimist').argv;
+		var testrunner = require('qunit');
+
+		testrunner.options.maxBlockDuration = 20000;
+		testrunner.options.coverage = true;
+		testrunner.options.log.tests = false;
+		testrunner.options.log.testing = false;
+		testrunner.options.log.assertions = false;
+		testrunner.options.log.summary = false;
+
+		var n = argv._.length || 1;
+		var failed = 0;
+		var done = 0;
+
+		var cb = function cb(err, report) {
+			if (err) {
+				console.log('error : some weird error happened, the only clue we have is this error message -> ' + err.message);
+				process.exit(-1);
+			}
+
+			failed += report.failed;
+			++done;
+			if (done === n) process.exit(failed);
+		};
+
+		var run = function run(item) {
+			testrunner.run({
+				code: {
+					path: path.normalize(codepath.join('/')),
+					namespace: name.replace('-', '')
 				},
-				tests : testpath.concat(item).join('/')
-			}, cb
-		);
+				tests: testpath.concat(item).join('/')
+			}, cb);
+		};
+
+		var i;
+
+		for (i = 0; i < argv._.length; ++i) {
+			argv._[i] = ['src', argv._[i] + '.js'].join('/');
+		}
+
+		if (argv._.length === 0) argv._.push('index.js');
+
+		for (i = 0; i < argv._.length; ++i) run(argv._[i]);
 	};
 
-
-	var i;
-
-	for (i = 0; i < argv._.length; ++i) {
-		argv._[i] = ['src', argv._[i]+'.js'].join('/');
-	}
-
-	if (argv._.length === 0) argv._.push('index.js');
-
-	for (i = 0; i < argv._.length; ++i) run(argv._[i]);
-};
-
-exports.test = test;
-
-})(typeof exports === 'undefined' ? this['pkg'] = {} : exports);
+	exports.test = test;
+})(typeof exports === 'undefined' ? undefined.pkg = {} : exports);
